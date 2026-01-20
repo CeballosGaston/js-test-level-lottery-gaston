cb44cc66c03862bb484122d5c6830720c5305b6e
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-01-20 11:57:18 +0100

    Main Functionality done

---
+++ dom.js
@@ -0,0 +1,27 @@
+"use strict";
+
+export function renderResult(container, object) {
+  const paragraph = document.createElement("p");
+
+  paragraph.textContent = `${object.message}`;
+
+  container.appendChild(paragraph);
+}
+
+export function renderHistory(array, container) {
+  const paragraph = document.getElementById("p");
+
+  let texto = array
+    .map((object) => {
+      return `Ingresado: ${object.data.userNumber} | Aleatorio: ${object.data.randomNumber} | Resultado: ${object.message}`;
+    })
+    .join("<br>");
+
+  paragraph.innerHTML = texto;
+
+  container.appendChild(paragraph);
+}
+
+export function errorValidate() {
+  return `You must enter a number from 1 to 10`;
+}
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// manager
+"use strict";
+
+
+import { validate, randomNumber, winOrNot, pushToHistory } from "./lotteryLogic.js";
+import { lotteryHistory } from "./history.js";
+import { renderHistory, renderResult, errorValidate } from "./dom.js";
+
+
+const numberInput = document.getElementById("number-input")
+const playButton = document.getElementById("playButton");
+const historyButton = document.getElementById("historyButton");
+const result = document.getElementById("result");
+
+
+
+
+
+playButton.addEventListener("click", () =>{
+try{
+if (!validate(numberInput.value)) return errorValidate();
+
+let randomNum = randomNumber();
+
+
+}catch{
+
+}
+
+
+
+})
